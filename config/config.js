893bb291d1370fa0d5f977f057afbbbf814e4327
CarolineBenn <lineybenn@gmail.com>
2016-05-20 14:57:06 +0100

    Changed app name

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port:     process.env.PORT || 3000,
-  database: process.env.MONGOLAB_URI || "mongodb://localhost/passport-jwt-boilerplate",
+  database: process.env.MONGOLAB_URI || "mongodb://localhost/reqresdatingfordevelopers",
   secret:   process.env.SECRET || "12Sas%3fdv&7"
 }
